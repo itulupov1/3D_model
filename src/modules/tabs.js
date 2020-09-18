2bfab50f69c29963ae245c2c77b29be35d1de80e
itulupov1 <itulupov1@yandex.ru>
2020-09-19 00:03:38 +0700

    fix(js)-again-lesson30

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -4,15 +4,15 @@ const tabs = () => {
 		tabContent = document.querySelectorAll('.service-tab');
 
 	const toggleTabContent = index => {
-		for (let i = 0; i < tabContent.length; i++) {
+		tabContent.forEach((item, i) => {
 			if (index === i) {
 				tab[i].classList.add('active');
-				tabContent[i].classList.remove('d-none');
+				item.classList.remove('d-none');
 			} else {
 				tab[i].classList.remove('active');
-				tabContent[i].classList.add('d-none');
+				item.classList.add('d-none');
 			}
-		}
+		});
 	};
 
 	tabHeader.addEventListener('click', event => {
